51d004435763083ff08670b1d17cb48421ee87b5
Rui Wang <wangruibistu@163.com>
2024-05-20 09:50:06 +0800

    Update kan_spectra_predict.py

--- kan_payne/kan_spectra_predict.py
+++ kan_payne/kan_spectra_predict.py
@@ -1,27 +1,11 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     custom_cell_magics: kql
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
-#   kernelspec:
-#     display_name: py311
-#     language: python
-#     name: python3
-# ---
 
-# %%
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import os, sys
 import sys
 
-sys.path.append("/home/wangr/code/efficient-kan/src/")
+# sys.path.append("/home/wangr/code/efficient-kan/src/")
 from efficient_kan import KAN
 
 # %%
